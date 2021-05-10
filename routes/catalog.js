a91b5c30799a744ad9dd4f5c58e09033a6686530
run-after <run.after@protonmail.com>
2021-05-10 15:00:23 -0500

    Add root route

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,22 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-//Require controller modules
+// Require controller modules.
 var book_controller = require('../controllers/bookController');
 var author_controller = require('../controllers/authorController');
 var genre_controller = require('../controllers/genreController');
 var book_instance_controller = require('../controllers/bookinstanceController');
 
-// BOOK ROUTES //
+/// BOOK ROUTES ///
 
-//GET catalog home page
-router.get('/book/create', book_controller.index);
+// GET catalog home page.
+router.get('/', book_controller.index);
 
-// GET requtest for createing a book. NOTE this must come before routes that display Book (uses id).
+// GET request for creating a Book. NOTE This must come before routes that display Book (uses id).
 router.get('/book/create', book_controller.book_create_get);
-
-// POST request for creating Book.
-router.post('/book/create', book_controller.book_create_post);
 
 // POST request for creating Book.
 router.post('/book/create', book_controller.book_create_post);
