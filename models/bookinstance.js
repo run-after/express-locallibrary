132fdc7c52c9400582c4362c0b2f2a743f99831d
run-after <run.after@protonmail.com>
2021-05-12 17:26:20 -0500

    Add luxon and format date for due back

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
+const { DateTime } = require('luxon');
+
 var BookInstanceSchema = new Schema(
   {
     book: { type: Schema.Types.ObjectId, ref: 'Book', required: true }, //reference to the associated book
@@ -17,6 +19,11 @@ BookInstanceSchema
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
 });
+
+BookInstanceSchema.virtual('due_back_formatted')
+  .get(function () {
+    return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+  });
 
 //Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
