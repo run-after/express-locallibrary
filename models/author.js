71d1e6216620e4f93ce2d268456024ece64f3311
run-after <run.after@protonmail.com>
2021-05-12 17:36:33 -0500

    Fix typo and add lexon to author model

--- models/author.js
+++ models/author.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
+const { DateTime } = require('luxon');
+
 var AuthorSchema = new Schema(
   {
     first_name: {type: String, required: true, maxLength: 100},
@@ -32,11 +34,13 @@ AuthorSchema
   return '/catalog/author/' + this._id;
 });
 
-AuthorSchema('date_of_birth_formatted').get(function () {
+AuthorSchema
+  .virtual('date_of_birth_formatted').get(function () {
   return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
 });
 
-AuthorSchema('date_of_death_formatted').get(function () {
+AuthorSchema
+  .virtual('date_of_death_formatted').get(function () {
   return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
 });
 
