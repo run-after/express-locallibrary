c7e67d5718a2b3a44a395b9b2549b893ecbbce05
run-after <run.after@protonmail.com>
2021-05-12 17:34:15 -0500

    Add formatted day of birth/death virtuals

--- models/author.js
+++ models/author.js
@@ -32,6 +32,14 @@ AuthorSchema
   return '/catalog/author/' + this._id;
 });
 
+AuthorSchema('date_of_birth_formatted').get(function () {
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+AuthorSchema('date_of_death_formatted').get(function () {
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+});
+
 //Export model
 module.exports = mongoose.model('Author', AuthorSchema);
 
