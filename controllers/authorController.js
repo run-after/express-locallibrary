fdb82a06ca3cef0966e43877ae4987d6470c9a66
run-after <run.after@protonmail.com>
2021-05-13 11:46:49 -0500

    Finish author detail controller method and view

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,6 @@
 var Author = require('../models/author');
+var Book = require('../models/book');
+var async = require('async');
 
 // Display list of all Authors.
 exports.author_list = function(req, res, next) {
@@ -15,7 +17,24 @@ exports.author_list = function(req, res, next) {
 
 // Display detail page for a spectific Author.
 exports.author_detail = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+  async.parallel({
+    author: function (callback) {
+      Author.findById(req.params.id)
+        .exec(callback)
+    },
+    authors_books: function (callback) {
+      Book.find({ 'author': req.params.id }, 'title summary')
+        .exec(callback)
+    },
+  }, function (err, results) {
+    if (err) { return next(err); }
+    if (results.author == null) {
+      var err = new Error('Author not found');
+      err.status = 404;
+      return next(err);
+    }
+    res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books })
+  });
 };
 
 // Display Author create form on GET.
